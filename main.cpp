3da12996b930d8b1786ba99dab9069567b5adee2
gab-thr <gabrielle.thurnherr@gmail.com>
2018-12-03 20:21:25 +0100

    introduction des constantes + début des commentaires

--- main.cpp
+++ main.cpp
@@ -20,6 +20,15 @@
 
 using namespace std;
 
+const int ZERO = 0;
+const int UN = 1;
+const int DIX = 10;
+const int CENT = 100;
+const int MILLE = 1000;
+
+const double AJUSTEMENT = 0.005;
+
+
 string montantEnVaudois(double montant);
 string montantDizaine(int montant);
 string montantCentaine(int montant, int millier);
@@ -39,86 +48,123 @@ int main() {
 	return 0;
 }
 
+/**
+ * [montantEnVaudois transcrit un nombre réel donne en toutes lettres]
+ * @param  montant [montant  un réel compris entre 0 et 999999.99 CHF]
+ * @return         [retourne une string ndiquant en vaudois le prix
+ *									en francs et centimes]
+ * Exemples:
+ * 12.30  -> "douze francs et trente centimes"
+ * 200.01 -> "deux cents francs et un centime"
+ * 180    -> "cent huitante francs"
+ * 1.80   -> "un franc et huitante centimes"
+ * 0.20   -> "vingt centimes"
+ * 0      -> "zéro franc"
+ */
 string montantEnVaudois(double montant) {
 
-	int montantEntier = (int)(montant + 0.005);
-	int montantDecimal = (int)((montant + 0.005 - montantEntier) * 100);
+	int montantEntier = (int)(montant + AJUSTEMENT);
+	int montantDecimal = (int)((montant + AJUSTEMENT - montantEntier) * CENT);
 
-	if (montantEntier == 0 && montantDecimal > 0)
-		return montantCentaine(montantDecimal, 0) + (montantDecimal == 1 ? " centime" : " centimes");
+	if (montantEntier == ZERO && montantDecimal > ZERO)
+		return montantCentaine(montantDecimal, ZERO) + (montantDecimal == UN ? " centime" : " centimes");
 
 	string montantTotal;
-	if (montantEntier < 1000)
-		if (montantEntier == 0 || montantEntier == 1)
-			montantTotal = montantCentaine(montantEntier, 0) + " franc";
+	if (montantEntier < MILLE)
+		if (montantEntier == ZERO || montantEntier == UN)
+			montantTotal = montantCentaine(montantEntier, ZERO) + " franc";
 		else
-			montantTotal = montantCentaine(montantEntier, 0) + " francs";
+			montantTotal = montantCentaine(montantEntier, ZERO) + " francs";
 	else
 		montantTotal = montantMillier(montantEntier) + " francs";
 
 
-	if (montantDecimal > 0)
-		montantTotal += " et " + montantCentaine(montantDecimal, 0) + (montantDecimal == 1 ? " centime" : " centimes");
+	if (montantDecimal > ZERO)
+		montantTotal += " et " + montantCentaine(montantDecimal, ZERO) + (montantDecimal == UN ? " centime" : " centimes");
+
+		cout << "decimal: " << montantDecimal << endl;
+cout << "entier: " << montantEntier << endl;
 
 	return montantTotal;
 }
 
+/**
+ * [montantDizaine retourne en lettres les nombres compris en 0 et 99]
+ * @param  montant [montant compris entre 0 et 99]
+ * @return         [retourne une string du nombre entre en lettres]
+ */
 string montantDizaine(int montant) {
 
-	if (montant >= 0 && montant <= 9)
+	if (montant >= ZERO && montant < DIX)
 		return unites(montant);
 
-	else if (montant >= 11 && montant <= 16)
+	else if (montant > DIX && montant <= 16)
 		return dizaines(montant);
-	else if (montant >= 10 && montant <= 99) {
-		int unite = montant % 10;
-		if (unite == 0)
+	else if (montant >= DIX && montant < CENT) {
+		int unite = montant % DIX;
+		if (unite == ZERO)
 			return dizaines(montant);
-		else if (unite == 1)
-			return dizaines(montant - unite) + " et " + unites(montant % 10);
-		else if (unite >= 1 && unite <= 9)
-			return dizaines(montant - unite) + "-" + unites(montant % 10);
+		else if (unite == UN)
+			return dizaines(montant - unite) + " et " + unites(montant % DIX);
+		else if (unite > ZERO && unite < DIX)
+			return dizaines(montant - unite) + "-" + unites(montant % DIX);
 	}
 }
 
+/**
+ * [montantCentaine description]
+ * @param  montant [description]
+ * @param  millier [description]
+ * @return         [description]
+ */
 string montantCentaine(int montant, int millier) {
 
-	if (montant < 100)
+	if (montant < CENT)
 		return montantDizaine(montant);
 
-	else if (montant >= 100 && montant <= 999) {
-		if (montant == 100)
+	else if (montant >= CENT && montant < MILLE) {
+		if (montant == CENT)
 			return centaines(montant);
-		else if (montant > 100) {
-			int dizaine = montant % 100;
-			if (dizaine == 0)
-				if (millier == 0)
-					return unites(montant / 100) + " " + centaines(dizaine);
+		else if (montant > CENT) {
+			int dizaine = montant % CENT;
+			if (dizaine == ZERO)
+				if (millier == ZERO)
+					return unites(montant / CENT) + " " + centaines(dizaine);
 				else
-					return unites(montant / 100) + " " + centaines(montant);
-			else if (dizaine > 0 && montant < 200)
+					return unites(montant / CENT) + " " + centaines(montant);
+			else if (dizaine > ZERO && montant < 200)
 				return centaines(montant - dizaine) + " " + montantDizaine(dizaine);
-			else if (dizaine > 0 && montant >= 200)
-				return unites(montant / 100) + " " + centaines(montant) + " " + montantDizaine(dizaine);
+			else if (dizaine > ZERO && montant >= 200)
+				return unites(montant / CENT) + " " + centaines(montant) + " " + montantDizaine(dizaine);
 		}
 	}
 }
 
+/**
+ * [montantMillier description]
+ * @param  montant [description]
+ * @return         [description]
+ */
 string montantMillier(int montant) {
 
-	int montantMille = montant / 1000;
-	int montantCent = montant % 1000;
+	int montantMille = montant / MILLE;
+	int montantCent = montant % MILLE;
 
-	if (montant == 1000)
+	if (montant == MILLE)
 		return "mille";
-	else if (montant >= 1001 && montant <= 1999)
-		return "mille " + montantCentaine(montantCent, 0);
-	else if (montantCent == 0)
+	else if (montant > MILLE && montant <= 1999)
+		return "mille " + montantCentaine(montantCent, ZERO);
+	else if (montantCent == ZERO)
 		return montantCentaine(montantMille, montantMille) + " mille ";
 	else
-		return montantCentaine(montantMille, montantMille) + " mille " + montantCentaine(montantCent, 0);
+		return montantCentaine(montantMille, montantMille) + " mille " + montantCentaine(montantCent, ZERO);
 }
 
+/**
+ * [unites permet de retourner une unité entre 0 et 9 en toutes lettres]
+ * @param  montant [nombre compris entre 0 et 9 à retourner en lettres]
+ * @return         [retourne une string d'une unité en toutes lettres]
+ */
 string unites(int montant) {
 
 	switch (montant) {
@@ -148,6 +194,11 @@ string unites(int montant) {
 	}
 }
 
+/**
+ * [dizaines détermine quelle dizaine en lettre il faut retourner, ainsi que les nombres entre 11 et 16]
+ * @param  montant [dizaine entière comprise entre 10 et 90 à retourner en lettres + dizaines spéciales (11-16)]
+ * @return         [retourne une string d'une dizaine entière ou une dizaine spéciale en lettres]
+ */
 string dizaines(int montant) {
 
 	switch (montant) {
@@ -187,6 +238,11 @@ string dizaines(int montant) {
 	}
 }
 
+/**
+ * [centaines retourne le mot "cent" accordé correctement]
+ * @param  montant [entier permettant de savoir quelle orthographe du mot "cent" utiliser]
+ * @return         [retourne une string correctement orthographiée]
+ */
 string centaines(int montant) {
 
 	switch (montant) {

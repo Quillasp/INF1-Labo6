472fa085876ae7b2598f39444d3cbca7cefd1d15
gab-thr <gabrielle.thurnherr@gmail.com>
2018-12-04 16:28:54 +0100

    fin des commentaires + ajout de constantes

--- main.cpp
+++ main.cpp
@@ -28,7 +28,16 @@ const int MILLE = 1000;
 
 const double AJUSTEMENT = 0.005;
 
+const string MILLIER = "mille";
+const string MONNAIE = "franc";
+const string CENTIME = "centime";
+const string ET = " et ";
 
+const char PLURIEL = 's';
+const char ESPACE = ' ';
+const char TIRET = '-';
+
+//Prototypes de foncions. Utilisés pour pouvoir ajouter des fonctions après le main (au lieu d'avant).
 string montantEnVaudois(double montant);
 string montantDizaine(int montant);
 string montantCentaine(int montant, int millier);
@@ -63,27 +72,34 @@ int main() {
  */
 string montantEnVaudois(double montant) {
 
+	// fait un arrondi au centieme a la decimale la plus proche peut importe le nombre entre
+	montant = (montant * CENT) / CENT;
+	//prend la partie entière du montant
 	int montantEntier = (int)(montant + AJUSTEMENT);
+	//prend la valeur décimale du montant, la converti en dizaine entiere (0.60 devient 60) et la stocke dans montantDecimal.
 	int montantDecimal = (int)((montant + AJUSTEMENT - montantEntier) * CENT);
-
+	//si la partie entière vaut zero et la partie decimale est plus grande que zero alors on traite que le partie decimale et la partie entière vaut 0.
 	if (montantEntier == ZERO && montantDecimal > ZERO)
-		return montantCentaine(montantDecimal, ZERO) + (montantDecimal == UN ? " centime" : " centimes");
+		 //Si le montantDecimal vaut 1 alors CENTIME est au singulier sinon CENTIME est au PLURIEL.
+		return montantCentaine(montantDecimal, ZERO) + (montantDecimal == UN ? ESPACE + CENTIME : ESPACE + CENTIME + PLURIEL);
 
 	string montantTotal;
+	//Si le montant entier est plus petit que 1000 on traite qu'une fois le nombre dans montantCentaine. L'idée c'est
+	//qu'avec des nombres plus grand on separe le nombre en deux et on traite chaque partie avec la même fonction (montantCentaine).
+	//Sinon traitement du nombre avec la fonction montantMillier.
 	if (montantEntier < MILLE)
+		//Traitement du cas où le montant vaut 0 ou 1 ce qui implique que franc est au singulier.
+		//Sinon franc est au PLURIEL.
 		if (montantEntier == ZERO || montantEntier == UN)
-			montantTotal = montantCentaine(montantEntier, ZERO) + " franc";
+			montantTotal = montantCentaine(montantEntier, ZERO) + ESPACE + MONNAIE;
 		else
-			montantTotal = montantCentaine(montantEntier, ZERO) + " francs";
+			montantTotal = montantCentaine(montantEntier, ZERO) + ESPACE + MONNAIE + PLURIEL;
 	else
-		montantTotal = montantMillier(montantEntier) + " francs";
+		montantTotal = montantMillier(montantEntier) + ESPACE + MONNAIE + PLURIEL;
 
-
+	//Traitement du cas où ni l'entier ni la décimale sont nulles. On ajoute au montantTotal tout simplement la décimale.
 	if (montantDecimal > ZERO)
-		montantTotal += " et " + montantCentaine(montantDecimal, ZERO) + (montantDecimal == UN ? " centime" : " centimes");
-
-		cout << "decimal: " << montantDecimal << endl;
-cout << "entier: " << montantEntier << endl;
+		montantTotal += ET + montantCentaine(montantDecimal, ZERO) + (montantDecimal == UN ? ESPACE + CENTIME : ESPACE + CENTIME + PLURIEL);
 
 	return montantTotal;
 }
@@ -91,73 +107,93 @@ cout << "entier: " << montantEntier << endl;
 /**
  * [montantDizaine retourne en lettres les nombres compris en 0 et 99]
  * @param  montant [montant compris entre 0 et 99]
- * @return         [retourne une string du nombre entre en lettres]
+ * @return         [retourne une string du nombre entier en en lettres]
  */
 string montantDizaine(int montant) {
-
+	//Si le montant  est plus grand que zero et plus petit que 10 on retourne la valeur donnée par la fonction unites.
 	if (montant >= ZERO && montant < DIX)
 		return unites(montant);
-
+	//Sinon si le montant est plus grand que 10 et plus petit ou égal à 16 on retourne la valeur donnée par la fonction dizaine.
 	else if (montant > DIX && montant <= 16)
 		return dizaines(montant);
+	//Si le montant vaut 10 et est comprise entre 17 et 99 on prend l'unité du montant.
+  //Si l'unité vaut 0 on traite le montant avec la fonction dizaine normalement. Si elle vaut 1
+  //on ajoute le ''et'' et sinon on ajoute le trait d'Union. Dans ces deux cas on traite les dizaines et unités séparement
+  //avec les fonctions dizaines et unites.
 	else if (montant >= DIX && montant < CENT) {
 		int unite = montant % DIX;
 		if (unite == ZERO)
 			return dizaines(montant);
 		else if (unite == UN)
-			return dizaines(montant - unite) + " et " + unites(montant % DIX);
+			return dizaines(montant - unite) + ET + unites(montant % DIX);
 		else if (unite > ZERO && unite < DIX)
-			return dizaines(montant - unite) + "-" + unites(montant % DIX);
+			return dizaines(montant - unite) + TIRET + unites(montant % DIX);
 	}
+	return "0";
 }
 
 /**
- * [montantCentaine description]
- * @param  montant [description]
- * @param  millier [description]
- * @return         [description]
+ * [montantCentaine traduit des nombres entiers compris entre]
+ * @param  montant [nombre entier à renvoyer en toutes lettres]
+ * @param  millier [nombre entier permettant de controler si le nombre entre par l'utilisateur est supérieur a 999]
+ * @return         [retourne une string de centaines en lettres]
  */
 string montantCentaine(int montant, int millier) {
 
+	// gere le cas ou le nombre est inferieur a 100
 	if (montant < CENT)
 		return montantDizaine(montant);
-
+	// gere les cas ou le nombre est compris entre 100 et 999
 	else if (montant >= CENT && montant < MILLE) {
+		// gere le cas ou le montant vaut 100
 		if (montant == CENT)
 			return centaines(montant);
+		// gere le cas ou le montant est suerieur a 100
 		else if (montant > CENT) {
+			// declaration d'une variable qui stocke les dizaines
 			int dizaine = montant % CENT;
+			// gere le cas des centaines ronde, cest a dire si 100 prend un "s" a la fin ou pas
 			if (dizaine == ZERO)
+				// si c'est des centaines de milliers 100 ne prendra pas de "s", exemple : deux cent mille
 				if (millier == ZERO)
-					return unites(montant / CENT) + " " + centaines(dizaine);
+					return unites(montant / CENT) + ESPACE + centaines(dizaine);
+				// sinon 100 prendra un "s", exemple deux cents
 				else
-					return unites(montant / CENT) + " " + centaines(montant);
+					return unites(montant / CENT) + ESPACE + centaines(montant);
+			// gere le cas des nombres compris entre 101 et 199 afin d'éviter le cas du "un cent.."
 			else if (dizaine > ZERO && montant < 200)
-				return centaines(montant - dizaine) + " " + montantDizaine(dizaine);
+				return centaines(montant - dizaine) + ESPACE + montantDizaine(dizaine);
+			// permet d'ecrire le nombre de centaines et le reste des nombres
 			else if (dizaine > ZERO && montant >= 200)
-				return unites(montant / CENT) + " " + centaines(montant) + " " + montantDizaine(dizaine);
+				return unites(montant / CENT) + ESPACE + centaines(montant) + ESPACE + montantDizaine(dizaine);
 		}
 	}
+	return "0";
 }
 
 /**
- * [montantMillier description]
- * @param  montant [description]
- * @return         [description]
+ * [montantMillier traduit des nombres supérieur ou égale à 1000 en toutes lettres]
+ * @param  montant [montant supérieur ou égale à 1000 à retranscrire en lettres]
+ * @return         [retourne une string contenant le nombre entier en lettres]
  */
 string montantMillier(int montant) {
 
+	//déclaration d'entiers afin de nous permettre de séparer la partie des milliers et la partie des centaines
 	int montantMille = montant / MILLE;
 	int montantCent = montant % MILLE;
 
+	// gère le cas d'un montant égal à mille
 	if (montant == MILLE)
-		return "mille";
+		return MILLIER;
+	// gère le cas d'un montant compris entre 1001 et 1999
 	else if (montant > MILLE && montant <= 1999)
-		return "mille " + montantCentaine(montantCent, ZERO);
+		return MILLIER + ESPACE + montantCentaine(montantCent, ZERO);
+	// gère le cas ou il n'y a pas de centaines après les milliers
 	else if (montantCent == ZERO)
-		return montantCentaine(montantMille, montantMille) + " mille ";
+		return montantCentaine(montantMille, montantMille) + ESPACE + MILLIER + ESPACE;
+	// gère tous les autres cas
 	else
-		return montantCentaine(montantMille, montantMille) + " mille " + montantCentaine(montantCent, ZERO);
+		return montantCentaine(montantMille, montantMille) + ESPACE + MILLIER + ESPACE + montantCentaine(montantCent, ZERO);
 }
 
 /**
